c062818ee38a95d61e1e4e224b61e002f2a5487d
josko <jklo.jurce@gmail.com>
2024-04-22 20:48:40 +0200

    add passenger-core

---
+++ passenger-core/src/main/java/com/josko/passenger/PassengerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.josko.passenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerCoreApplication.class, args);
+	}
+
+}
